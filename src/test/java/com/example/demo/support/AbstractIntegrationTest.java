664420fc7a7a60bf75ae82c4faec4ad18ee76400
Filus <filus.pa@gmail.com>
2023-03-05 10:22:26 +0100

    Step 6 - add Redis support provided by Testcontainers to our tests

--- src/test/java/com/example/demo/support/AbstractIntegrationTest.java
+++ src/test/java/com/example/demo/support/AbstractIntegrationTest.java
@@ -8,15 +8,22 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = {
         "spring.datasource.url=jdbc:tc:postgresql:14-alpine://testcontainers/workshop"
 })
 public class AbstractIntegrationTest {
+    static final GenericContainer redis = new GenericContainer("redis:6-alpine")
+            .withExposedPorts(6379);
+
     protected RequestSpecification requestSpecification;
 
     @LocalServerPort
     protected int localServerPort;
+
 
     @BeforeEach
     public void setUpAbstractIntegrationTest() {
@@ -28,5 +35,12 @@ public class AbstractIntegrationTest {
                         MediaType.APPLICATION_JSON_VALUE
                 )
                 .build();
+    }
+
+    @DynamicPropertySource
+    public static void configureRedis(DynamicPropertyRegistry registry) {
+        redis.start();
+        registry.add("spring.redis.host", redis::getHost);
+        registry.add("spring.redis.port", redis::getFirstMappedPort);
     }
 }
