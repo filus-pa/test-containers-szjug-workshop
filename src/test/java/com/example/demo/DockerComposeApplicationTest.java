a376517574db7f1ca24bd943e98f0152dd6ee7ac
Filus <filus.pa@gmail.com>
2023-03-05 19:26:48 +0100

    Step 10 - Migrating to individual Testcontainers objects
    S

--- src/test/java/com/example/demo/DockerComposeApplicationTest.java
+++ src/test/java/com/example/demo/DockerComposeApplicationTest.java
@@ -9,12 +9,16 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.testcontainers.containers.DockerComposeContainer;
+import org.testcontainers.containers.*;
 import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.images.builder.ImageFromDockerfile;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+import org.testcontainers.utility.MountableFile;
 
 import java.io.File;
+import java.nio.file.Paths;
 
 import static io.restassured.RestAssured.given;
 import static org.awaitility.Awaitility.await;
@@ -23,11 +27,48 @@ import static org.hamcrest.Matchers.is;
 @Testcontainers
 public class DockerComposeApplicationTest  {
 
+    static Network network = Network.newNetwork();
+
+    @Container
+    static final GenericContainer redis = new GenericContainer("redis:6-alpine")
+            .withExposedPorts(6379)
+            .withNetwork(network)
+            .withNetworkAliases("redis");
+
+    @Container
+    static final KafkaContainer kafka = new KafkaContainer (
+            DockerImageName.parse("confluentinc/cp-kafka:6.2.1"))
+            .withNetwork(network)
+            .withNetworkAliases("kafka");
+
+    @Container
+    static final PostgreSQLContainer postgres = new PostgreSQLContainer<>("postgres:14-alpine")
+            .withCopyFileToContainer(MountableFile.forClasspathResource("/talks-schema.sql"), "/docker-entrypoint-initdb.d/")
+            .withNetwork(network)
+            .withNetworkAliases("db");
+
     @Container
     static DockerComposeContainer<?> composeContainer = new DockerComposeContainer<>(new File("docker-compose.yml"))
             .withLocalCompose(true)
             .withExposedService("app_1", 8080)
             .waitingFor("app_1", Wait.forHttp("/actuator/health"));
+
+    @Container
+    static final GenericContainer appContainer = new GenericContainer<>(
+            new ImageFromDockerfile()
+                    .withFileFromPath("Dockerfile", Paths.get("Dockerfile"))
+                    .withFileFromPath("build/libs/test-containers-szjug-workshop.jar", Paths.get("build/libs/test-containers-szjug-workshop.jar"))
+    )
+            .withExposedPorts(8080)
+            .withEnv("SPRING_REDIS_HOST", "redis")
+            .withEnv("SPRING_REDIS_PORT", "6379")
+            .withEnv("SPRING_KAFKA_BOOTSTRAP_SERVERS", "BROKER://kafka:9092")
+            .withEnv("SPRING_DATASOURCE_URL", "jdbc:postgresql://db:5432/test")
+            .withEnv("SPRING_DATASOURCE_USERNAME", "test")
+            .withEnv("SPRING_DATASOURCE_PASSWORD", "test")
+            .withNetwork(network)
+            .waitingFor(Wait.forHttp("/actuator/health"))
+            .dependsOn(redis, kafka, postgres);
 
     protected RequestSpecification requestSpecification;
 
@@ -35,7 +76,7 @@ public class DockerComposeApplicationTest  {
     public void setUpAbstractIntegrationTest() {
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
         requestSpecification = new RequestSpecBuilder()
-                .setBaseUri(String.format("http://%s:%d", composeContainer.getServiceHost("app_1", 8080), composeContainer.getServicePort("app_1", 8080)))
+                .setBaseUri(String.format("http://%s:%d", appContainer.getHost(), appContainer.getFirstMappedPort()))
                 .addHeader(
                         HttpHeaders.CONTENT_TYPE,
                         MediaType.APPLICATION_JSON_VALUE
