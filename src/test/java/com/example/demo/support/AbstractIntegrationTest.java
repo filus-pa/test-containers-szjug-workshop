d11e55a7d3130dd093e1155781fec29d2b8e3f32
Filus <filus.pa@gmail.com>
2023-03-05 10:00:58 +0100

    Step 4 - added test container with Postgresql db

--- src/test/java/com/example/demo/support/AbstractIntegrationTest.java
+++ src/test/java/com/example/demo/support/AbstractIntegrationTest.java
@@ -2,7 +2,9 @@ package com.example.demo.support;
 
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = {
+        "spring.datasource.url=jdbc:tc:postgresql:14-alpine://testcontainers/workshop"
+})
 public class AbstractIntegrationTest {
 
 }
