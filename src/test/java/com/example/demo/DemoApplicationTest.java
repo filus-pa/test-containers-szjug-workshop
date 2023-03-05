82c98baa09fc4ac5e3b04b455645dd9b5c43ff0c
Filus <filus.pa@gmail.com>
2023-03-05 09:45:45 +0100

    Step 3 - dummy test to run app in default context

---
+++ src/test/java/com/example/demo/DemoApplicationTest.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import com.example.demo.support.AbstractIntegrationTest;
+import org.junit.jupiter.api.Test;
+
+public class DemoApplicationTest extends AbstractIntegrationTest {
+
+    @Test
+    public void contextLoads() {
+        //dummy test to see if Spring detects H2 db
+    }
+}
