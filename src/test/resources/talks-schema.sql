d4d2579801e09a40f511d7908cd0187c38e1ee5e
Filus <filus.pa@gmail.com>
2023-03-05 11:15:21 +0100

    Step 9 - migrating db with Flyaway

--- src/test/resources/talks-schema.sql
+++ src/test/resources/talks-schema.sql
@@ -3,13 +3,3 @@ CREATE TABLE IF NOT EXISTS talks(
   title VARCHAR(255) NOT NULL,
   PRIMARY KEY (id)
 );
-
-INSERT
-  INTO talks (id, title)
-  VALUES ('testcontainers-integration-testing', 'Modern Integration Testing with Testcontainers')
-  ON CONFLICT do nothing;
-
-INSERT
-  INTO talks (id, title)
-  VALUES ('flight-of-the-flux', 'A look at Reactor execution model')
-  ON CONFLICT do nothing;
