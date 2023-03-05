7535a5f82e7af3a837bf04ccadc82f8cb6873aab
Filus <filus.pa@gmail.com>
2023-03-05 10:59:40 +0100

    Step 9 - move Postgresql initialization and setup to explicit defined container

--- src/test/java/com/example/demo/DemoApplicationTest.java
+++ src/test/java/com/example/demo/DemoApplicationTest.java
@@ -1,12 +1,18 @@
 package com.example.demo;
 
+import com.example.demo.repository.TalksRepository;
 import com.example.demo.support.AbstractIntegrationTest;
 import io.restassured.filter.log.LogDetail;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import static io.restassured.RestAssured.given;
 
 public class DemoApplicationTest extends AbstractIntegrationTest {
+
+    @Autowired
+    TalksRepository talks;
 
     @Test
     public void contextLoads() {
@@ -21,5 +27,10 @@ public class DemoApplicationTest extends AbstractIntegrationTest {
                 .then()
                 .statusCode(200)
                 .log().ifValidationFails(LogDetail.ALL);
+    }
+
+    @Test
+    public void talks() {
+        Assertions.assertTrue(talks.exists("testcontainers-integration-testing"));
     }
 }
