0dca63aa19ad29f087e3f78af34df747397fa8b1
Filus <filus.pa@gmail.com>
2023-03-05 10:15:33 +0100

    Step 5 - add failed test for RestAssured api test

--- src/test/java/com/example/demo/support/AbstractIntegrationTest.java
+++ src/test/java/com/example/demo/support/AbstractIntegrationTest.java
@@ -1,10 +1,32 @@
 package com.example.demo.support;
 
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = {
         "spring.datasource.url=jdbc:tc:postgresql:14-alpine://testcontainers/workshop"
 })
 public class AbstractIntegrationTest {
+    protected RequestSpecification requestSpecification;
 
+    @LocalServerPort
+    protected int localServerPort;
+
+    @BeforeEach
+    public void setUpAbstractIntegrationTest() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        requestSpecification = new RequestSpecBuilder()
+                .setPort(localServerPort)
+                .addHeader(
+                        HttpHeaders.CONTENT_TYPE,
+                        MediaType.APPLICATION_JSON_VALUE
+                )
+                .build();
+    }
 }
